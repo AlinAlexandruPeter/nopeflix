3f2e7e877892e796f1960578acd039b876db66a3
alinpeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2022-10-27 06:20:43 +0300

    Update requests.js

--- src/js/requests.js
+++ src/js/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "bb432c86a681943a07c80b99b5f3c237";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
